a66a7cd958ffd83e1a70ac539e1e2de896e44573
unknown <gores530@PC3-3.helix.ida.liu.se>
2015-12-16 11:46:28 +0100

    pathfinder should work now (tested in python)

--- Game/Model/Logic/PathFinder.cs
+++ Game/Model/Logic/PathFinder.cs
@@ -13,7 +13,6 @@ namespace Game.Model.Logic
         private int[,]      costs;
         private int         width;
         private int         height;
-        private Board       board;
 
         public PathFinder(int width, int height){
             this.width  = width;
@@ -22,6 +21,14 @@ namespace Game.Model.Logic
             resetCosts();
         }
 
+        // ärv av denna klass för att skapa en callback-funktion till pathfinder
+        public abstract class CanPass
+        {
+            protected CanPass() { }
+            public abstract bool check(Point position);
+        }
+
+        // hjälpfunktioner:
         private void resetCosts(){
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
@@ -29,18 +36,12 @@ namespace Game.Model.Logic
                 }
             }
         }
-
-        private bool validIndex(int x, int y)
-        {
+        private bool validIndex(int x, int y){
             return (0 <= x && x < width) && (0 <= y && y < height);
         }
-
-
-        public class CanPass {
-            protected CanPass() {}
-            public virtual bool check(Point position) { throw new NotImplementedException("override this method"); }
+        private bool validIndex(Point p){
+            return validIndex(p.X, p.Y);
         }
-
         private List<Point> getNeighbourPositions( Point pos ){
             return new List<Point>
             {
@@ -54,28 +55,36 @@ namespace Game.Model.Logic
         private void updateCosts( Point pos, CanPass canPass ){
             resetCosts();
             // ingen spelare får gå utanför kartan:
-            if (!validIndex(pos.X, pos.Y)) return;
+            if (!validIndex(pos.X, pos.Y)) throw new ArgumentException("invalid start position");
 
             costs[pos.X, pos.Y] = 0;
             List<Point> position_queue = new List<Point>{ pos };
 
             while (position_queue.Count != 0)
             {
+                // ta bort första elementet i kön och lägg det i current
                 Point current = position_queue[0];
                 position_queue.RemoveAt(0);
+
                 int new_cost = costs[current.X,current.Y] + 1;
+
                 foreach (Point p in getNeighbourPositions(current))
                 {
+                    // om en granne har en högre kostnad än vad vi vill sätta så ändrar vi den
                     if ( validIndex(p.X,p.Y) && canPass.check(p) && new_cost < costs[p.X,p.Y] )
                     {
+                        // ändra kostnad och lägg över nya koordinaten längst bak i kön
                         costs[p.X, p.Y] = new_cost;
-                        position_queue.Insert(position_queue.Count - 1, p);
+                        position_queue.Add(p);
                     }
                 }
             }
         }
 
         public Point getClosestPointOfInterest(Point pos, List<Point> POIs, CanPass canPass){
+            if (!validIndex(pos)) throw new ArgumentException("POI is invalid");
+            foreach (Point POI in POIs) { if (!validIndex(POI)) throw new ArgumentException("found invalid point in POIs"); }
+
             updateCosts(pos, canPass);
 
             Point closest_point = Point.Error;
@@ -83,7 +92,6 @@ namespace Game.Model.Logic
 
             foreach ( Point POI in POIs ){
                 int POI_cost = costs[POI.X, POI.Y];
-
                 if ( POI_cost < closest_cost ){
                     closest_point = POI;
                     closest_cost  = POI_cost;
@@ -92,77 +100,57 @@ namespace Game.Model.Logic
             return closest_point;
         
         }
-        public List<Point> getPathWithExactCost(Point current_pos, Point goal, int exact_cost, CanPass canPass){
-            List<Point> path = new List<Point>();
-            resetCosts();
-            updateCosts(goal, canPass);
-
-            Point[] neighbour_offset = new Point[]
-            {
-                new Point( 1, 0 ), new Point( 0, 1 ),
-                new Point(-1, 0 ), new Point( 0,-1 ),
-            };
-
-            foreach ( Point pt in neighbour_offset ){
-                Point new_pos = current_pos + pt;
-                if (costs[new_pos.X, new_pos.Y] == exact_cost - 1)
+        public List<Point> getPathWithExactCost(Point from, Point to, int exact_cost, CanPass canPass){
+            updateCosts(to, canPass);
+            // blockera rutan vi står på:
+            costs[from.X, from.Y] = int.MaxValue;
+            // kolla om någon av grannarna har en väg till målet:
+            foreach ( Point neighbour in getNeighbourPositions(from) ){
+                if ( validIndex(neighbour) && costs[neighbour.X, neighbour.Y] == exact_cost - 1)
                 {
-                    // blockera rutan vi stod på:
-                    costs[current_pos.X, current_pos.Y] = int.MaxValue;   // istället för detta kan man alternativt använda en ändrad callbackfuktion
-                    // hitta kortaste väg från den nya punkten:
-                    path = getShortestPath( current_pos, goal, canPass, false );
-                    if (path.Count != 0){
-                        path.Insert(0, new_pos);
+                    List<Point> path = getShortestPath( neighbour, to, canPass, false );
+                    // notera att path innehåller en ruta mer än den totala längden på vägen.
+                    if (path != null && path.Count == exact_cost){
+                        path.Insert(0, from);
+                        return path;
                     }
-                    return path;
                 }
             }
-
-            return new List<Point>();
+            // om ingen av grannarna har en väg med rätt kostnad så returneras inget:
+            return null;
         }
 
-        public List<Point> getShortestPath(Point current_pos, Point goal, CanPass canPass)
+        public List<Point> getShortestPath(Point from, Point to, CanPass canPass)
         {
-            return getShortestPath(current_pos, goal, canPass, true);
+            return getShortestPath(from, to, canPass, true);
         }
 
-        private List<Point> getShortestPath( Point current_pos, Point goal, CanPass canPass, bool recalcPaths ){
-            List<Point> path = new List<Point>();
-
-            if (costs[current_pos.X, current_pos.Y] == int.MaxValue) return path;
-            if (current_pos == goal) return path;
-
+        private List<Point> getShortestPath( Point from, Point to, CanPass canPass, bool recalcPaths ){
+            if (!validIndex(from) || !validIndex(to)) throw new ArgumentException("invalid position");
             // spelplanen behöver inte alltid räknas om:
-            if (recalcPaths){
-                resetCosts();
-                updateCosts(goal, canPass);
-            }
+            if (recalcPaths) updateCosts(to, canPass);
+            // om rutan inte kan nås så returneras ingen väg:
+            if (costs[from.X, from.Y] == int.MaxValue) return null;
 
-            Point[] neighbour_offsets = new Point[]
-            {
-                new Point( 1, 0 ), new Point( 0, 1 ),
-                new Point(-1, 0 ), new Point( 0,-1 ),
-            };
+            List<Point> path = new List<Point> { from };
+            if (from == to) return path;
+            Point last_pos = Point.Error;
+            while ( last_pos != path[path.Count-1] ) {
+                last_pos = path[path.Count - 1];
 
-            /* om det inte fungerar helt så kolla *
-             * gärna om i ska börja på 1 eller 0  */
-            Point min_pos = current_pos;
-            do {
-                Point last_point = min_pos;
-                foreach ( Point neighbour_offset in neighbour_offsets ){
-                    Point neighbour = min_pos + neighbour_offset;
-                    if ( costs[neighbour.X, neighbour.Y] < costs[min_pos.X, min_pos.Y] ){
-                        min_pos = neighbour;
+                foreach ( Point neighbour in getNeighbourPositions(last_pos) ){
+                    if ( costs[neighbour.X, neighbour.Y] < costs[last_pos.X, last_pos.Y] ){
+                        path.Add(neighbour);
                     }
                 }
-                // ifall koden inte kan hitta vägen ger vi endast en tom väg
-                if (min_pos == last_point){
-                    return new List<Point>();
-                } else {
-                    path.Add(min_pos);
-                }
-            } while ( min_pos != goal );
-            return path;
+            }
+
+            if (path != null && path.Contains(to)){
+                return path;
+            } else {
+                return null;
+            }
+            
         }
     }
 }
